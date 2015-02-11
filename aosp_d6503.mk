5aeb7f2c719e29ac332705e084ac7680b6ecace0
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-02-11 13:06:30 +0100

    sirius: [squash] Minor updates for Kernel 3.10
    remove legacy boot logo
    wifi: overlay configurations for bcmdhd
    set memory parameters for 3GB devices
    
    Change-Id: I408793979d54781fcf6032115f61e55cc2a493d1

--- aosp_d6503.mk
+++ aosp_d6503.mk
@@ -20,10 +20,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
 $(call inherit-product-if-exists, device/sony/shinano/device.mk)
 $(call inherit-product-if-exists, vendor/sony/sirius/sirius-vendor.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
 #$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4339/device-bcm.mk)
 
 PRODUCT_COPY_FILES += \
-    device/sony/sirius/rootdir/logo.rle:root/logo.rle \
     device/sony/sirius/rootdir/system/etc/BCM4339.hcd:system/etc/firmware/BCM43xx.hcd \
     device/sony/sirius/rootdir/system/etc/wifi/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
     device/sony/sirius/rootdir/system/etc/sensors_calib.conf:system/etc/sensors_calib.conf \
